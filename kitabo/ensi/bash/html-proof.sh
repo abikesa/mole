930c7da69989e85c991fa1988b55f4910c6ed2ae
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:09 -0400

    kitabo/ensi/bash/html-proof.sh

---
+++ kitabo/ensi/bash/html-proof.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+make site 
+bundle exec htmlproofer --allow-hash-href \
+--assume-extension ./_site \
+--url-ignore "/\/apple-touch*.*/,/\/images/logo/favicon.ico/,/#*/" \
+--disable-external \
+--only_4xx 
+
+# flick 20250409213603-Rmxp
+# flick 20250409214209-ajNo
