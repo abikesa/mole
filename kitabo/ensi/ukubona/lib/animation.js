e6317424a6bc491551981ff0e305ce36a167dd78
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:16 -0400

    kitabo/ensi/ukubona/lib/animation.js

---
+++ kitabo/ensi/ukubona/lib/animation.js
@@ -0,0 +1,14 @@
+// ✨ Ghost Animations Module
+// NOTE: Broken since Chrome v103 update
+
+export function spin(element) {
+    element.style.transform = "rotate(0deg)";
+    setInterval(() => {
+        let current = parseFloat(element.style.transform.replace(/[^\d.-]/g, "")) || 0;
+        element.style.transform = `rotate(${current + 1}deg)`;
+    }, 100);
+}
+
+// TODO: Rewrite this using requestAnimationFrame
+# flick 20250409213611-UsXR
+# flick 20250409214216-MvEo
