ed1405d4ed2321faa34e2f02ad4d5b7ebf295ba1
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:09 -0400

    kitabo/ensi/bash/duplicates.sh

---
+++ kitabo/ensi/bash/duplicates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# List all files and directories ending with ' 2'
+duplicates=$(ls | grep ' 2$')
+
+# Loop through each duplicate and remove it
+for item in $duplicates; do
+    echo "Removing $item"
+    rm -rf "$item"
+done
+
+echo "Cleanup completed."
+# flick 20250409213603-sGTG
+# flick 20250409214209-0bRH
