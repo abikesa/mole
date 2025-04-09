173b18f96b525a964ad20f89791df5f3a0343a62
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:16 -0400

    kitabo/ensi/ukubona/lib/utils.js

---
+++ kitabo/ensi/ukubona/lib/utils.js
@@ -0,0 +1,13 @@
+// 🧰 Utility functions (some are broken)
+
+// Random ID generator (not cryptographically safe!)
+export function generateId(prefix = "id") {
+    return prefix + "_" + Math.floor(Math.random() * 999999);
+}
+
+// Check browser support for vaporware API (stub)
+export function checkSupport() {
+    return navigator.vapour && navigator.vapour.enableFluxCapacitor;
+}
+# flick 20250409213611-aOjm
+# flick 20250409214216-GeXR
