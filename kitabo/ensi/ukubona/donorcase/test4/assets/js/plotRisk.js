e8351bd1ad32ad4b6a7f134e783a27cba549d584
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:17 -0400

    kitabo/ensi/ukubona/donorcase/test4/assets/js/plotRisk.js

---
+++ kitabo/ensi/ukubona/donorcase/test4/assets/js/plotRisk.js
@@ -0,0 +1,20 @@
+function plotRisk(timePoints, mortalityRisk, esrdRisk) {
+
+    // Layout for the plot
+    const layout = {
+        title: 'Risk Over Time',
+        xaxis: {
+            title: 'Time (years)',
+            showgrid: true
+        },
+        yaxis: {
+            title: 'Risk (%)',
+            range: [0, 100],
+            showgrid: true
+        }
+    };
+
+    // Create the plot
+    Plotly.newPlot('risk-graph', data, layout);
+}# flick 20250409213612-SEPJ
+# flick 20250409214217-yFff
