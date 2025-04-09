b522cb678cb9867b870c36c056ce1fe36ae6e4b7
abikesa <abikesa.sh@gmail.com>
2025-04-09 17:42:10 -0400

    kitabo/ensi/bash/jbp-https.sh

---
+++ kitabo/ensi/bash/jbp-https.sh
@@ -0,0 +1,7 @@
+# jb push the local directory
+rm -rf _build
+pip install -r requirements.txt && jb build .
+cp -r part1/app/* _build/html/part1/app
+cd ~/documents/rhythm
+local/kitabo/ensi/scripts/jbp-https.sh# flick 20250409213603-jNab
+# flick 20250409214210-JKvx
